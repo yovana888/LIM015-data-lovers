3368fe5585245c88e6d56b7e13296bcaa1e2f493
gisselP <https://github.com/gisselP>
2021-06-29 22:32:53 -0500

    funcionalidad en JS
    descansa >:v

--- src/data.js
+++ src/data.js
@@ -1,9 +1,20 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+/* export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
 };
+ */
+const data= {
+  getData: function() {
+    fetch('ghibli.json')
+    .then(res=> res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+};
+export default data;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
+/* import { example } from './data.js';
 console.log(example, data);
+ */
+import dataGhibli from './data/ghibli/ghibli.js';
+/* import data from './data.js'; */
+const getAlldata= dataGhibli.films;
 
+window.addEventListener("load", function() {
+    for (let i = 0; i < getAlldata.length; i++) {
+        var addHtml ='<div class="card">'
+        +'<div class="card-contenido">'
+        +'<div class="card-img">'
+        +'<img src="https://static.wikia.nocookie.net/studio-ghibli/images/d/db/My_Neighbor_Totoro.jpg">'
+        +'</div>'
+        +'<div class="text-card ">'
+        +'<label>'+getAlldata[1].title+'</label>'
+        +'<p class="text-sumary">Lorem ipsum dolor sit amet adipisicing elit. Fugit ducimus, veniam fugiat aliquid autem modi odio id necessitatibus distinctio nulla beatae fugiat aliquid autem modi ...</p>'
+        +' <label class="text-sumary">Director: Hayao Miyazaki</label>'
+        +'<label class="text-sumary">Productor: Hayao Miyazaki</label>'
+        +'</div>'
+        +'</div>'
+        +'</div>';
+    }
+    document.getElementById("cardsList").innerHTML = addHtml;  
+
+ });
